e702ba239461eb520b697bff52d441caf9a3b5f3
rightmeow <jinhedev@gmail.com>
2018-08-08 22:21:18 -0700

    Title: Finishes palindrome with html

--- main.js
+++ main.js
@@ -1,10 +1,23 @@
 let Phrase = require("rightmeow-palindrome")
 
-let string = prompt("Please enter a string for palindrome testing:")
-let phrase = new Phrase(string)
+function palindromeTester(event) {
+  event.preventDefault()
 
-if (phrase.isPalindrome()) {
-  alert(`"${phrase.content} is a palindrome!"`)
-} else {
-  alert(`"${phrase.content}" is NOT a palindrome.`)
+  let phrase = new Phrase(string)
+  let palindromeResult = document.querySelector("#palindromeResult")
+
+  if (phrase.isPalindrome()) {
+    // alert(`"${phrase.content} is a palindrome!"`)
+    palindromeResult.innerHTML = `"${phrase.content}" is a palindrome`
+  } else {
+    // alert(`"${phrase.content}" is NOT a palindrome.`)
+    palindromeResult.innerHTML = `"${phrase.content}" is not a palindrome`
+  }
 }
+
+docment.addEventListener("DOMContentLoaded", function() {
+  let tester = document.querySelector("#palindromeTester")
+  tester.addEventListener("submit", function() {
+    palindromeTester(event)
+  })
+})
